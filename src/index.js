038eafbe6908b6e3eaaca24c981a41fe4b074ad5
Boris Dinkevich <your_email@example.com>
2017-03-15 14:58:42 +0200

    Added add recipe

--- src/index.js
+++ src/index.js
@@ -2,9 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App() {
-  return React.createElement('h1', null, 'Hello!');
+
+const Header = ({ title }) => (
+  <h1>{ title }</h1>
+);
+
+const Recipe = ({ title }) => (
+  <li onClick={ ()=> console.log('YEy') }>{ title }</li>
+);
+
+const Recipes = ({ recipes }) => (
+  <ul>
+    {
+      recipes.map(recipe =>
+        <Recipe title={ recipe } key={ recipe }/>
+      )
+    }
+  </ul>
+);
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipes: ['Waffel', 'Omelette', 'Pancake']
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      recipes: this.state.recipes.concat(this.titleElem.value)
+    });
+
+    this.titleElem.value = '';
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Header title="Recipes:" />
+          <Recipes recipes={ this.state.recipes } />
+
+          <form onSubmit={ this.handleSubmit }>
+            <input ref={ (elem) => this.titleElem = elem } type="text"/>
+            <button>Add</button>
+          </form>
+
+        </div>
+      </div>
+    )
+  }
 }
+
+window.App = App;
 
 ReactDOM.render(
   React.createElement(App),
