3c02a0e165a8a3cbbde43805f4ccbeb24d91c309
Boris Dinkevich <your_email@example.com>
2017-05-18 14:10:06 +0300

    Temp 1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App() {
-  return React.createElement('h1', null, 'Hello!');
+class Recipes extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipes: ['Omelette', 'Pancake', 'Falafel', 'Waffles']
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      recipes: this.state.recipes.concat(this.titleElem.value)
+    });
+
+    this.titleElem.value = '';
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          { this.state.recipes.map(str => <li key={ str }>{ str } </li>) }
+        </ul>
+
+        <form onSubmit={ this.handleSubmit }>
+          <input ref={ elem => this.titleElem = elem } type="text"/>
+          <button>add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
+const Header = ({ msg }) => (
+  <h1>{ msg }</h1>
+);
+
+const App = () => (
+  <div>
+    <Header msg="hi" color="blue"/>
+    <h2>Ezugi</h2>
+    <Recipes />
+  </div>
+);
+
 ReactDOM.render(
-  React.createElement(App),
+  <App />,
   document.getElementById('root')
 );
